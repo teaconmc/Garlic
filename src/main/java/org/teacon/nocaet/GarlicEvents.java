6aa79daa9d654fdc9eecaa334f607e8027c21ba4
IzzelAliz <csh2001331@126.com>
2022-07-15 12:49:31 +0800

    Various fix

--- src/main/java/org/teacon/nocaet/GarlicEvents.java
+++ src/main/java/org/teacon/nocaet/GarlicEvents.java
@@ -26,37 +26,39 @@ public class GarlicEvents {
     @SubscribeEvent
     public void playerJoin(EntityJoinWorldEvent event) {
         if (!event.getWorld().isClientSide() && event.getEntity() instanceof ServerPlayer player) {
-            player.inventoryMenu.addSlotListener(new ContainerListener() {
-
-                @Override
-                public void slotChanged(AbstractContainerMenu pContainerToSend, int pSlotInd, ItemStack pStack) {
-                    if (pContainerToSend == player.inventoryMenu && !(pContainerToSend.getSlot(pSlotInd) instanceof ResultSlot)) {
-                        if (pStack.is(GarlicRegistry.FLAME_TAG)) {
-                            pStack.getCapability(GarlicCapability.bind())
-                                .filter(it -> it.bindTo(player))
-                                .flatMap(it -> player.getCapability(GarlicCapability.flames()).resolve())
-                                .ifPresent(it -> grantAndBroadcast(player, it, pStack));
-                        }
-                    }
-                }
-
-                @Override
-                public void dataChanged(AbstractContainerMenu pContainerMenu, int pDataSlotIndex, int pValue) {
-                }
-            });
+            player.inventoryMenu.addSlotListener(new FlameListener(player));
             player.getCapability(GarlicCapability.flames()).ifPresent(it ->
                 GarlicChannel.getChannel().send(PacketDistributor.PLAYER.with(() -> player), new SyncFlamesPacket(it.getGranted()))
             );
         }
     }
 
-    private void grantAndBroadcast(ServerPlayer player, FlameAdvancement advancement, ItemStack stack) {
+    private static void grantAndBroadcast(ServerPlayer player, FlameAdvancement advancement, ItemStack stack) {
         if (advancement.add(stack.getItem().getRegistryName())) {
             var text = new TranslatableComponent("nocaet.flame.grant", player.getDisplayName(), stack.getDisplayName());
             ServerLifecycleHooks.getCurrentServer().getPlayerList().broadcastMessage(text, ChatType.SYSTEM, Util.NIL_UUID);
             GarlicChannel.sendProxy(PacketDistributor.PLAYER.with(() -> player),
                 new AddFlamePacket(player.getUUID(), stack.getItem().getRegistryName(), stack.getDisplayName()));
             GarlicChannel.getChannel().send(PacketDistributor.PLAYER.with(() -> player), new SyncFlamesPacket(advancement.getGranted()));
+        }
+    }
+
+    private record FlameListener(ServerPlayer player) implements ContainerListener {
+
+        @Override
+        public void slotChanged(AbstractContainerMenu pContainerToSend, int pSlotInd, ItemStack pStack) {
+            if (pContainerToSend == player.inventoryMenu && !(pContainerToSend.getSlot(pSlotInd) instanceof ResultSlot)) {
+                if (pStack.is(GarlicRegistry.FLAME_TAG)) {
+                    pStack.getCapability(GarlicCapability.bind())
+                        .filter(it -> it.bindTo(player))
+                        .flatMap(it -> player.getCapability(GarlicCapability.flames()).resolve())
+                        .ifPresent(it -> grantAndBroadcast(player, it, pStack));
+                }
+            }
+        }
+
+        @Override
+        public void dataChanged(AbstractContainerMenu pContainerMenu, int pDataSlotIndex, int pValue) {
         }
     }
 }
